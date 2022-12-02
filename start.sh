2447f8e2fd7cf0a78616aad420e1d9dfbb854b06
indu <70087467+lazyindu@users.noreply.github.com>
2022-12-02 22:28:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/link-search-bot---mdisk-search-bot /Mdisk-Search-Bot
+git clone https://github.com/lazyindu/newbbbb /Mdisk-Search-Bot
 cd /Mdisk-Search-Bot
 pip3 install -r requirements.txt
-echo "Starting Bot....GreyMatter_Bots"
+echo "Starting Bot..."
 python3 main.py
